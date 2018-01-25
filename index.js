3c8f79b58ddbc60e105a8a59af99a6333ce7c0f3
Christian Timberlake <timberlakece1@my.palmbeachstate.edu>
2018-01-25 01:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma (string) {
-  if (string.toLowerCase() === string)
-  return "I can't hear you!";
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  } else if (string.toUpperCase() === string){
+    return "YES INDEED!";
+  } else {
+    return "I love you, too.";
+  }
+}
